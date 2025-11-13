28529a00a0b4d5e98058144ab934cf4e79f2517e
DARCK-CREATOR <yadadaniel12@gmail.com>
2025-11-13 14:56:47 +0100

    requirementsss

--- school/settings.py
+++ school/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&z=thx$=cbtk+&9^kq%!o0n2d@wxiq@csjjwwap)$z%77%=wbr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
