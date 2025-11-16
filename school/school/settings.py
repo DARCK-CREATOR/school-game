f5bf0c052e82b1890cc056025bf87826954b4116
DARCK-CREATOR <yadadaniel12@gmail.com>
2025-11-16 18:57:58 +0100

    heu

--- school/school/settings.py
+++ school/school/settings.py
@@ -133,6 +133,7 @@ MEDIA_ROOT = BASE_DIR / 'images'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATIC_ROOT = BASE_DIR /"staticfiles"
 
 # Default primary key field type
